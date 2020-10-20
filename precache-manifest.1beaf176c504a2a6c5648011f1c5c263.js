1c3d3b3d1fb5f79cb182e2915839b064aca7c4a8
phyislou <phyislou@hotmail.com>
2020-10-20 11:22:32 +0800

    Updates

---
+++ precache-manifest.1beaf176c504a2a6c5648011f1c5c263.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56eaa2edabe2283c09b8713f619c3bc8",
+    "url": "/idlegacha.github.io/index.html"
+  },
+  {
+    "revision": "dd141635fcdc0d614eef",
+    "url": "/idlegacha.github.io/static/css/main.5f71c29b.chunk.css"
+  },
+  {
+    "revision": "4a02bcedbb7e46e408d5",
+    "url": "/idlegacha.github.io/static/js/2.63ab2077.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/idlegacha.github.io/static/js/2.63ab2077.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd141635fcdc0d614eef",
+    "url": "/idlegacha.github.io/static/js/main.78db9a64.chunk.js"
+  },
+  {
+    "revision": "8a691b5f37dd47493e29",
+    "url": "/idlegacha.github.io/static/js/runtime-main.3cd93ea5.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/idlegacha.github.io/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/idlegacha.github.io/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/idlegacha.github.io/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/idlegacha.github.io/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/idlegacha.github.io/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
